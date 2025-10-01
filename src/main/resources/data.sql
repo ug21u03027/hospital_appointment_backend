660dc2db68b92728578491ddec3694138921bde7
ujjwal <ug21u03027@gmail.com>
2025-10-01 23:13:32 +0530

    start from scratch

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT IGNORE INTO users (id, name, username, password, role) VALUES
-  (1,'admin','admin','admin','ADMIN'),
-  (2,'user','user1','pass1','USER'),
-  (3,'ashamehta','Dr. Asha Mehta','dasha','DOCTOR');
-
